732b3434f0f811eff834e1896535fcf5df01aca0
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-02-27 07:52:49 -0800

    k

--- src/MakeitHalaal/Dashboard/Homepage.js
+++ src/MakeitHalaal/Dashboard/Homepage.js
@@ -32,8 +32,7 @@ function Homepage(props) {
                             <div  className="flex flex-col rounded-md items-center justify-center w-[400px] md:w-[500px]  shadow-2xl my-8 ">
 
                          
-              <img src={`data:image/png;base64,${btoa(
-              String.fromCharCode(...new Uint8Array(info.IDcardimage.data.data)))}`} 
+              <img src={info.IDcardimage.url} 
               alt="..." className='rounded-md object-cover' />
 
                               
